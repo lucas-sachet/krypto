b602ffaf590e7695917887b14dc8931ea0d8508c
Lucas Sachet <lucasachet@gmail.com>
2022-01-24 22:36:57 -0300

    create chart

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -3,11 +3,26 @@ import styled from 'styled-components';
 import { BsThreeDotsVertical } from 'react-icons/bs'
 import { coins } from '../static/coins'
 import Coin from './Coin';
+import BalanceChart from './BalanceChart';
 
 function Portfolio() {
     return (
         <Wrapper>
             <Content>
+                <Chart>
+                    <div>
+                        <Balance>
+                            <BalanceTitle>
+                                Portfolio balance
+                            </BalanceTitle>
+                            <BalanceValue>
+                                {`${'$' + 46000}`}
+                            </BalanceValue>
+                        </Balance>
+                    </div>
+                    <BalanceChart />
+                </Chart>
+            
             <PortfolioTable>
                 <TableItem>
                     <Title>Your Assets</Title>
@@ -50,6 +65,24 @@ const Content = styled.div`
     width: 100%;
     max-width: 1000px;
     padding: 2rem 1rem;
+`
+
+const Chart = styled.div`
+    border: 1px solid #282b2f;
+    padding: 1rem 2rem;
+`
+
+const Balance = styled.div``
+
+const BalanceTitle = styled.div`
+    color: #8a919e;
+    font-size: 0.9rem;
+`
+
+const BalanceValue = styled.div`
+    font-size: 1.8rem;
+    font-weight: 700;
+    margin: 0.5rem 0;
 `
 
 const PortfolioTable = styled.div`
